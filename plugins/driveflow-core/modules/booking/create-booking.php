591162192a8f327733ebf77d0d83e5b650c7bcba
Fred <huertaserick@yahoo.ie>
2026-07-01 18:00:58 +0100

    Create booking via custom REST API and store in WordPress

--- plugins/driveflow-core/modules/booking/create-booking.php
+++ plugins/driveflow-core/modules/booking/create-booking.php
@@ -2,8 +2,10 @@
 
 function driveflow_create_booking(WP_REST_Request $request)
 {
+    // json token
     $data = $request->get_json_params();
 
+    // validation
     if (empty($data['name'])) {
         return new WP_Error(
             'missing_name',
@@ -28,19 +30,31 @@ function driveflow_create_booking(WP_REST_Request $request)
         );
     }
 
+    // sanitize
     $name = sanitize_text_field($data['name']);
     $service = sanitize_text_field($data['service']);
     $date = sanitize_text_field($data['date']);
     $time = sanitize_text_field($data['time']);
 
+    // booking creation
+    $post_id = wp_insert_post([
+        'post_type' => 'booking',
+        'post_status' => 'publish',
+        'post_title' => $name . ' - ' . $service,
+    ]);
+
+    // error checking 
+    if (is_wp_error($post_id)) {
+        return new WP_Error(
+            'booking_failed',
+            'Unable to create booking.',
+            ['status' - 500]
+        );
+    }
+
     return [
         'success' => true,
-        'message' => 'Booking handler connected',
-        'received' => [
-            'name' => $name,
-            'service' => $service,
-            'date' => $date,
-            'time' => $time,
-        ]
+        'message' => 'Booking created successfully',
+        'booking_id' => $post_id,
     ];
 }
